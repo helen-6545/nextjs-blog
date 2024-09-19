1599a3f6b9ddc74a64c2f0b785af28458f012eb9
helen-6545 <122414895+helen-6545@users.noreply.github.com>
2024-09-18 20:23:15 -0400

    yayy i did it

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -1,5 +1,48 @@
-export default function gallery(){
-    return(<div>
-        hey
-    </div>)
+'use client';
+import React from "react";
+import Home from './page'
+import {pics} from './page'
+let state = 0
+
+
+
+
+
+
+
+
+export default function gallery(props: any){
+
+const [image, setImage] = React.useState("/pictures/craneIllustrationResized.jpg");
+
+function Previous(){
+    state=state-1
+    if(state==-1){
+        state=3
+    }
+    setImage(pics[state])
+    console.log({image})
 }
+
+function Next(){
+    state=state+1
+    if(state==4){
+        state=0;
+    }
+    setImage(pics[state])
+    console.log({image})
+    
+}
+
+    return(<div>
+        
+        <button onClick={Previous}>Previous</button>
+        <button onClick={Next}>Next</button>
+
+        <img src = {image}/>
+        
+    </div>)
+
+
+}
+
